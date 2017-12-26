c3a4f3bcfc4cc5b158100734e94a85d73a0efb89
Webxus <inetrudix@gmail.com>
2017-12-27 01:51:55 +1000

    вроде работает

--- js/index.js
+++ js/index.js
@@ -10,28 +10,33 @@
 
 const audio = document.getElementsByTagName('audio');
 
-const clap = document.getElementsByClassName('drum-kit__drum key-clap');
-const hihat = document.getElementsByClassName('drum-kit__drum key-hihat');
-const kick = document.getElementsByClassName('drum-kit__drum key-kick');
-const openhat = document.getElementsByClassName('drum-kit__drum key-openhat');
-const boom = document.getElementsByClassName('drum-kit__drum key-boom');
-const ride = document.getElementsByClassName('drum-kit__drum key-ride');
+const clap = document.getElementsByClassName('drum-kit__drum key-clap'),
+hihat = document.getElementsByClassName('drum-kit__drum key-hihat'),
+kick = document.getElementsByClassName('drum-kit__drum key-kick'),
+openhat = document.getElementsByClassName('drum-kit__drum key-openhat'),
+boom = document.getElementsByClassName('drum-kit__drum key-boom'),
+ride = document.getElementsByClassName('drum-kit__drum key-ride');
 
 clap[0].onclick = function() {
 	audio[0].play();
 }
+
 hihat[0].onclick = function() {
 	audio[1].play();
 }
+
 kick[0].onclick = function() {
 	audio[2].play();
 }
+
 openhat[0].onclick = function() {
 	audio[3].play();
 }
+
 boom[0].onclick = function() {
 	audio[4].play();
 }
+
 ride[0].onclick = function() {
 	audio[5].play();
 }
